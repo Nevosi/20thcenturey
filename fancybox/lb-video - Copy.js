dda2ac844bf5b2f317dbf4bff682742654157870
Nevosi <jblatecky@jblatecky.com>
2015-06-22 10:14:01 -0400

    clean up and added cenema page

---
+++ fancybox/lb-video - Copy.js
@@ -0,0 +1,10 @@
+$(".fancybox")
+		.attr('rel', 'gallery')
+		.fancybox({
+				openEffect  : 'none',
+				closeEffect : 'none',
+				nextEffect  : 'none',
+				prevEffect  : 'none',
+				padding     : 0,
+				margin      : [20, 60, 20, 60] // Increase left/right margin
+		});
---
+++ js/ramdomimg.js
@@ -0,0 +1,9 @@
+var random_images_array = ["20th_century_leonard_nimoy_banner1.jpg", "20th_century_leonard_nimoy_banner2.jpg","20th_century_leonard_nimoy_banner3.jpg","20th_century_leonard_nimoy_banner4.jpg"];
+
+	function getRandomImage(imgAr, path) {
+    path = path || 'images/'; // default path here
+    var num = Math.floor( Math.random() * imgAr.length );
+    var img = imgAr[ num ];
+    var imgStr = '<input type="image" id="banner_ad" src="' + path + img + '" alt = "Donate to help Fight COPD" width="635" height="100">';
+    document.write(imgStr);
+}
